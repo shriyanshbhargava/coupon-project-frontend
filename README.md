b4239c9bfc90f6685e2413540e0da4dbbd4faefa
Deepansh Bhargava <deepansh940@gmail.com>
2023-10-06 18:11:22 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# coupon-project-frontend
+## Getting Started
+
+- Install all the dependencies using `yarn install`
+- Run `yarn dev`
+- Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/app/add-coupon/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function AddCoupon() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h2>Add Coupon Page</h2>
+      </div>
+    </main>
+  )
+}
